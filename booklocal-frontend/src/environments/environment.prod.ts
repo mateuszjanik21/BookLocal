dcb65f26b8457c049f408f75f39ba7ad15572226
Mateusz Janik <janik-mat@o2.pl>
2025-08-14 12:02:14 +0200

    adding azure connection

--- booklocal-frontend/src/environments/environment.prod.ts
+++ booklocal-frontend/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
     production: true,
-    apiUrl: 'https://adresprodukcyjny.com/api'
+    apiUrl: 'https://booklocal-api-gja0begeg4gfbfcj.polandcentral-01.azurewebsites.net/api'
 };
